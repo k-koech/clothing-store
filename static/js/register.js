1817f28330c482fb1fa31725ad8d87d63ca7fb47
Moringa School <kelvin.bett@student.moringaschool.com>
2021-06-29 20:14:58 +0300

    function register and var declarations

---
+++ static/js/register.js
@@ -0,0 +1,8 @@
+function register() {
+    var email = document.getElementById("email");
+    var userName = document.getElementById("username");
+    var phoneNumber = document.getElementById("phonenumber");
+    var password = document.getElementById("password");
+    var confirmPassword = document.getElementById("conpassword");
+
+}
