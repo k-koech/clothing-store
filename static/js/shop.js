38be4161c3b169bd1e0e36610c36d4a286856195
k-koech <kelvin.koech@student.moringaschool.com>
2021-06-30 20:04:17 +0300

    updated shopping products

--- static/js/shop.js
+++ static/js/shop.js
@@ -1,10 +1,21 @@
-// rating
+// Show ratings div when the product is overed
+$(document).ready(function(){
+    $( ".item" ).mouseenter(function() 
+    {
+        var closestImage = $(this).closest(".item");
+        closestImage.next(".ratings").slideDown(100);
+      })
+      $(".cols" ).mouseleave(function() {
+        $(".ratings" ).hide();
+    });
+    });
+
+
 $(document).ready(function() {  
-    
+
 $('.ratings').each(function() 
 {
 
-   
         $("#st1").click(function() {  
             $(".fa-star").css("color", "black");  
             $("#st1").css("color", "green");  
@@ -30,6 +41,6 @@ $('.ratings').each(function()
             $("#st1, #st2, #st3, #st4, #st5").css("color", "green");  
 
         });  
-    });  
+});  
 
 });
